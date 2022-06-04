34fae00c78791a67143afb5939d54d827a37e8bb
Jakin Kim <104137221+Jakin-Kim@users.noreply.github.com>
2022-06-04 12:35:46 +0900

    Update Two-Sum.js

--- LeetCode/Two-Sum.js
+++ LeetCode/Two-Sum.js
@@ -1,1 +1,21 @@
+// I got accepted when I run the test
+// However, once I submit button the result status came out wrong... don't know why... lol
 
+let twoSum = function(nums, target) {
+    // Given: an array of integers(num) and an integer(target)
+    // Return: indices of two numbers in the array of 'nums' adding to 'target'
+    
+    // dual for loop
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = 1; j < nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                nums[j + 1];
+            }
+            if(nums[i] + nums[j] === target) {
+                result.push(i) && result.push(j);
+                return result;
+            }
+        }
+    }
+};
