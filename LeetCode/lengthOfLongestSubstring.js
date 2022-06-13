cd9bc0a7ced1fcd510f472823c2e8cfd640c5afd
Jakin Kim <104137221+Jakin-Kim@users.noreply.github.com>
2022-06-13 22:11:30 +0900

    Create lengthOfLongestSubstring.js

---
+++ LeetCode/lengthOfLongestSubstring.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    // input: string(s)
+    // output: the length of the longest substring without repeating characters
+    
+    // 1. to keep track of all the unique characters
+    let map = {};
+    // 2. for initializing the length of max as zero
+    let maxLen = 0; 
+    // 3. to keep track of substring length
+    let start = 0;
+    // 4. iterating string(s) of each letter 
+    for (let end = 0; end < s.length; end++) {
+        let lastChar = s[end];
+        // 5. checking if the lastest character is in the 'map'
+        if (map[lastChar] === undefined) {
+            map[lastChar] = 0; // initializing it as zero       
+            // point1: The value(here above is 0) defines the each character to see if it's counted or not.
+        }
+        map[lastChar] += 1;
+        while(map[lastChar] > 1) {
+            let firstChar = s[start];
+            map[firstChar] -= 1;
+            start += 1;          
+        }
+        maxLen = Math.max(maxLen, end - start + 1);    
+    }
+    return maxLen;
+};
