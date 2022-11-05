0a8f09424c2bc6aadb259dca351d31bd41cad5ef
Jakin-Kim <104137221+Jakin-Kim@users.noreply.github.com>
2022-11-06 01:25:34 +0900

    Update rotateMatrix.js

--- Daily-Coding_Codestates/rotateMatrix.js
+++ Daily-Coding_Codestates/rotateMatrix.js
@@ -13,19 +13,19 @@
 //   return output;
 // };
 
-// [ matrix[3][0], matrix[2][0], matrix[1][0], matrix[0][0] ]
-// [ matrix[3][1], matrix[2][1], matrix[1][1], matrix[0][1] ]
-// [ matrix[3][2], matrix[2][2], matrix[1][2], matrix[0][2] ]
-// [ matrix[3][3], matrix[2][3], matrix[1][3], matrix[0][3] ]
+// [ matrix[3][0], matrix[2][0], matrix[1][0], matrix[0][0] ] -> [ output[0][0], output[0][1], output[0][2], output[0][3] ],
+// [ matrix[3][1], matrix[2][1], matrix[1][1], matrix[0][1] ] -> [ output[1][0], output[1][1], output[1][2], output[1][3] ],
+// [ matrix[3][2], matrix[2][2], matrix[1][2], matrix[0][2] ] -> [ output[2][0], output[2][1], output[2][2], output[2][3] ],
+// [ matrix[3][3], matrix[2][3], matrix[1][3], matrix[0][3] ] -> [ output[3][0], output[3][1], output[3][2], output[3][3] ],
 
 const rotateMatrix = function (matrix, rotateNum = 1) {
-  // rotateNum 이 0일 수 있으므로 아래와 같은 초기화는 지양해야 함
+  // rotateNum 이 0일 수도(회전을 하지 않을 수도) 있으므로 아래와 같은 초기화는 지양해야 함
   // rotateNum = rotateNum || 1
   const N = matrix.length;
-  // 빈 배열을 입력받을 수 있다.
+  // 빈 배열을 입력받을 수도 있기 때문에 && 사용한 조건문 활용.
   const M = matrix[0] && matrix[0].length;
 
-  rotateNum = rotateNum % 4;
+  rotateNum = rotateNum % 4; // 4면체이기 때문에 4번 돌면 원래 상태로 되돌아온다.
   // 회전하지 않는다.
   if (rotateNum === 0) return matrix;
 
@@ -36,10 +36,9 @@ const rotateMatrix = function (matrix, rotateNum = 1) {
   for (let row = 0; row < RC[0]; row++) {
     rotated[row] = [];
     for (let col = 0; col < RC[1]; col++) {
-      if (rotateNum === 1) rotated[row][col] = matrix[N - col - 1][row];
-      else if (rotateNum === 2)
-        rotated[row][col] = matrix[N - row - 1][M - col - 1];
-      else rotated[row][col] = matrix[col][M - row - 1];
+      if (rotateNum === 1) rotated[row][col] = matrix[N - col - 1][row]; // 홀수번 회전 시
+      if (rotateNum === 2) rotated[row][col] = matrix[N - row - 1][M - col - 1]; // 짝수번 회전 시
+      else rotated[row][col] = matrix[col][M - row - 1]; // rotateNum === 3인 경우까지 밖에 없음.
     }
   }
 
