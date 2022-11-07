2eb117d095144555d778bd20b2980ca915d9f893
Jakin-Kim <104137221+Jakin-Kim@users.noreply.github.com>
2022-11-07 15:49:45 +0900

    Update insertionSort.js

--- Daily-Coding_Codestates/insertionSort.js
+++ Daily-Coding_Codestates/insertionSort.js
@@ -1,40 +1,28 @@
-// naive solution
-// const insertionSort = function (arr) {
-//   let sorted = [arr[0]];
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] >= sorted[i - 1]) {
-//       sorted.push(arr[i]);
-//     } else {
-//       for (let j = 0; j < i; j++) {
-//         if (arr[i] <= sorted[j]) {
-//           const left = sorted.slice(0, j);
-//           const right = sorted.slice(j);
-//           sorted = left.concat(arr[i], right);
-//           break;
-//         }
-//       }
-//     }
-//   }
-
-//   return sorted;
-// };
-
-const insertionSort = function (arr, transform = (item) => item) {
+const insertionSort = function (arr, callback = (item) => item) {
+  // POINT1: 두번째 인자로 만든 callback 함수를 없애고 아래 풀이에서 callback 함수로 wrapping 된 친구들을 풀어주거나,
+  // POINT2: 두번째 인자로 callback 함수를 사용할 때 callback 함수의 인자를 그대로 리턴해주는 함수로 작성하면 된다.
+  let left, right;
   let sorted = [arr[0]];
-  for (let i = 1; i < arr.length; i++) {
-    if (transform(arr[i]) >= transform(sorted[i - 1])) {
-      sorted.push(arr[i]);
+
+  // arr = [4,2,1,6,3,8,7,5,9] // sorted = [1] (1회차 loop)
+  for(let i = 1; i < arr.length; i++) { 
+    if(callback(arr[i]) < callback(sorted[0])) {
+      sorted.unshift(arr[i])
     } else {
-      for (let j = 0; j < i; j++) {
-        if (transform(arr[i]) <= transform(sorted[j])) {
-          const left = sorted.slice(0, j);
-          const right = sorted.slice(j);
+      for(let j = 0; j < sorted.length; j++) {
+        if(callback(arr[i]) < callback(sorted[j])) {
+          left = sorted.slice(0,j);
+          right = sorted.slice(j);
           sorted = left.concat(arr[i], right);
+          break;
+        } 
+        if(callback(arr[i]) >= callback(sorted[sorted.length - 1])) { // sorted의 마지막 요소(가장 큰 요소)와 비교하기
+          sorted.push(arr[i]);
           break;
         }
       }
     }
   }
-
   return sorted;
+  
 };
