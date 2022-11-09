cb6c4551ba6246f12d2543bc74f383aef5646ea5
Jakin-Kim <104137221+Jakin-Kim@users.noreply.github.com>
2022-11-09 13:12:13 +0900

    Update mergeSort.js

--- Daily-Coding_Codestates/mergeSort.js
+++ Daily-Coding_Codestates/mergeSort.js
@@ -1,30 +1,32 @@
-const merge = function (left, right) {
-  let merged = [];
-  let leftIdx = 0,
-    rightIdx = 0;
-  const size = left.length + right.length;
-
-  for (let i = 0; i < size; i++) {
-    if (leftIdx >= left.length) {
-      merged.push(right[rightIdx]);
-      rightIdx++;
-    } else if (rightIdx >= right.length || left[leftIdx] <= right[rightIdx]) {
-      merged.push(left[leftIdx]);
+// 정렬하고 병합할 함수 생성
+const merge = (leftArr, rightArr) => {
+  const sorted = []; 
+  const totalLength = leftArr.length + rightArr.length;
+  let leftIdx = 0, 
+      rightIdx = 0;
+  for(let i = 0; i < totalLength; i++) { // 단순히 비교횟수를 위해 for문 사용
+    if(rightIdx === rightArr.length || leftArr[leftIdx] < rightArr[rightIdx]) {
+      sorted.push(leftArr[leftIdx]);
       leftIdx++;
     } else {
-      merged.push(right[rightIdx]);
+      sorted.push(rightArr[rightIdx]);
       rightIdx++;
     }
   }
-
-  return merged;
+  return sorted;  
 };
 
 const mergeSort = function (arr) {
-  if (arr.length < 2) return arr;
-  const middle = parseInt(arr.length / 2);
-  const left = mergeSort(arr.slice(0, middle));
-  const right = mergeSort(arr.slice(middle));
-  const merged = merge(left, right);
-  return merged;
+  
+  // base case
+  if(arr.length <= 1) return arr;
+
+  // recursive case
+  const midIdx = Math.floor(arr.length / 2);
+  const left = arr.slice(0, midIdx);
+  const right = arr.slice(midIdx);
+  const leftSorted = mergeSort(left);
+  const rightSorted = mergeSort(right);
+
+  return merge(leftSorted, rightSorted)
 };
