c3bcedc370b19964d086991b5b0893ad346da0d5
Jakin-Kim <kimyagin94@gmail.com>
2022-11-30 22:27:30 +0900

    Update maxSubarraySum.js

--- JavaScript 알고리즘 & 자료구조 마스터클래스/섹션5: 문제해결 패턴/maxSubarraySum.js
+++ JavaScript 알고리즘 & 자료구조 마스터클래스/섹션5: 문제해결 패턴/maxSubarraySum.js
@@ -2,11 +2,12 @@
 // which accepts an array of integers and a number called n.
 // The function should calculate the maximum sum of n consecutive elements in the array.
 
+// 내가 풀이한 방법
 function maxSubarraySum(arr, n) {
 	if(!arr.length) return null;
 
 	let subArr = [];
-	let maxSum = 0;
+	let maxSum = -Infinity; // 배열의 요소가 음수일 수도 있기 때문에
 	for(let i = 0; i < arr.length; i++) {
 		for(let j = i; j < n+i; j++) {
 			subArr.push(arr[j]);
@@ -21,4 +22,21 @@ function maxSubarraySum(arr, n) {
 		}
 	}
 	return maxSum;
+}
+
+// 해결책1
+function maxSubarraySum(arr, num) { // [1,2,5,2,8,1,5], 4
+	if(num > arr.length) return null;
+
+	let max = -Infinity;
+	for(let i = 0; i < arr.length - num + 1; i++) {
+		temp = 0;
+		for(let j = 0; j < num; j++) {
+			temp += arr[i+j];
+		}
+		if(temp > max) {
+			max = temp;
+		}
+	}
+	return max;
 }
