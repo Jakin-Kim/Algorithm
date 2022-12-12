4573e8b877c26e2c2e0dff3b09a633094e3dbab3
Jakin-Kim <kimyagin94@gmail.com>
2022-12-12 19:14:51 +0900

    Update searchNaiveString.js

--- JavaScript 알고리즘 & 자료구조 마스터클래스/섹션10: 검색 알고리즘/searchNaiveString.js
+++ JavaScript 알고리즘 & 자료구조 마스터클래스/섹션10: 검색 알고리즘/searchNaiveString.js
@@ -1,3 +1,4 @@
+// solution1. O(N)
 function searchNaiveString(longStr, shortStr) {
     let longIdx = 0;
     let shortIdx = 0;
@@ -19,4 +20,16 @@ function searchNaiveString(longStr, shortStr) {
     }
 
     return count;
+}
+
+// solution2. O(N^2)
+function searchNaiveString(longStr, shortStr) {
+	let count = 0;
+	for(let i = 0; i < longStr.length; i++) {
+		for(let j = 0; j < shortStr.length; j++) {
+			if(shortStr[j] !== longStr[i+j]) break;
+			if(j === short.length - 1) count++;
+		}
+	}
+	return count;
 }
