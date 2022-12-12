3be91649193c341e469cd8443802f4cad1a74857
Jakin-Kim <kimyagin94@gmail.com>
2022-12-12 18:11:25 +0900

    Create binarySearch.js

---
+++ JavaScript 알고리즘 & 자료구조 마스터클래스/섹션10: 검색 알고리즘/binarySearch.js
@@ -0,0 +1,21 @@
+function binarySearch(sortedArr, val){
+  let leftIdx = 0;
+  let rightIdx = sortedArr.length - 1;
+  let middleIdx = Math.ceil((leftIdx + rightIdx) / 2)
+  
+  while(leftIdx < rightIdx) {
+    middleIdx = Math.ceil((leftIdx + rightIdx) / 2)
+    
+    if(val < sortedArr[middleIdx]) {
+        rightIdx = middleIdx;
+    }
+    
+    if(val > sortedArr[middleIdx]) {
+        leftIdx = middleIdx;
+    }
+    
+    if(val === sortedArr[middleIdx]) return middleIdx;
+  }
+  
+  return -1;
+}
