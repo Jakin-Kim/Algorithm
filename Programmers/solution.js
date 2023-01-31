8473e936121e7a94d79dfad1db564bc059b480e1
Jakin-Kim <kimyagin94@gmail.com>
2023-01-31 09:35:42 +0900

    Create solution.js

---
+++ Programmers/solution.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = [];
+    let stringifiedN = String(n);
+    for(let i = stringifiedN.length - 1; i >= 0; i--) {
+        answer.push(Number(stringifiedN[i]));
+    }
+    return answer;
+}
