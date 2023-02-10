e62c46aa3dcec1b4299932a5a70ed70a3c6be4f5
Jakin-Kim <kimyagin94@gmail.com>
2023-02-10 18:37:34 +0900

    Create newUser.js

---
+++ Programmers/newUser.js
@@ -0,0 +1,14 @@
+function newUser(new_id) {
+    let rec_id = new_id
+    rec_id = rec_id
+        .toLowerCase()
+        .replace(/[^a-z0-9-_.]/g, '')
+        .replace(/\.+/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .replace(/^$/, 'a')
+        .slice(0, 15)
+        .replace(/\.$/, '');
+    return rec_id.length > 2
+        ? rec_id
+        : rec_id + rec_id[rec_id.length - 1].repeat(3 - rec_id.length);
+}
